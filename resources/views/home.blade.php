9fc887c8be457a42ee46883942926b26adabdfde
Dario0122 <dario.di.cuia@gmail.com>
2022-01-19 18:34:37 +0100

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,21 @@
 
 @extends('layouts.main')  
 @section('main-content')  
-    <h1>hello world</h1>
-    <h2>sono Dario</h2>
+    <main>
+        <section class="container">
+            <div class="cards d-flex flex-wrap">
+                @foreach ($cards as $card)
+                    <div class="card">
+                        <div class="img-card">
+                            <img src="{{ $card['thumb'] }}" alt="{{ $card['series'] }}">
+                        </div>
+                        <h6>{{ $card['series'] }}</h4>
+                    </div>
+                @endforeach
+            </div>
+            <div class="btn">
+                <button class="text-center">LOAD MORE</button>
+            </div>
+        </section>
+    </main>
 @endsection
