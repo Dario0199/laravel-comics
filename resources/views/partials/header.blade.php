29ce053463ca1c85bd98fe9d81287ba68cc31936
Dario0122 <dario.di.cuia@gmail.com>
2022-01-19 15:47:05 +0100

    setup

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <a href="{{ route('home') }}">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="DC logo">
+    </a>
+
+    <ul>
+        <li>
+            <a href="">COMICS</a>
+        </li>
+        <li>
+            <a href="">NEWS</a>
+        </li>
+    </ul>
+</header>
