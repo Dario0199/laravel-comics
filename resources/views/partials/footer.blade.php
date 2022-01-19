506e447b2e07909b233559db02380d5fc8eeb03a
Dario0122 <dario.di.cuia@gmail.com>
2022-01-19 21:18:51 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,26 @@
-
 <footer>
-    <hr>
-    <h1>footer</h1>
+    <div class="container d-flex justify-content-between">
+        <button>SIGN-UP NOW!</button>
+        <div class="right d-flex align-items-center">
+            <h5>FOLLOW US</h5>
+            <ul class="d-flex align-items-center">
+                <li>
+                <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                </li>
+                <li>
+                <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                </li>
+                <li>
+                <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                </li>
+                <li>
+                <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                </li>
+                <li>
+                <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+                </li>
+            
+            </ul>
+        </div>
+    </div>
 </footer>
