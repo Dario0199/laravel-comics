34b9cadbd273231c65b1d9401ad3ef42022e0769
Dario0122 <dario.di.cuia@gmail.com>
2022-01-19 17:07:51 +0100

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,19 @@
-<header>
-    <a href="{{ route('home') }}">
-        <img src="{{ asset('images/dc-logo.png') }}" alt="DC logo">
-    </a>
+<header class="d-flex container ">
+   <div class="logo">
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC logo">
+        </a>
+   </div>
 
-    <ul>
-        <li>
-            <a href="">COMICS</a>
-        </li>
-        <li>
-            <a href="">NEWS</a>
-        </li>
-    </ul>
+    <div class="nav">
+        <ul class="d-flex align-items-center">
+            <li>
+                <a href="{{ route('home') }}">COMICS</a>
+            </li>
+            <li>
+                <a href="">NEWS</a>
+            </li>
+        </ul>
+    </div>
+    <hr>
 </header>
